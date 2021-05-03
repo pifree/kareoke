24bfc0e469066f11fce0ee2ff5250c87603ff625
lbarcl <ersoybarcin@gmail.com>
2021-05-03 20:17:10 +0300

    Fix#3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const v1 = require('./routes/v1/index')
+const v1 = require('./routes/v1/index.js')
 
 app.use('/v1', v1)
 app.listen(3000, () => {console.log(`http://localhost:3000`)})

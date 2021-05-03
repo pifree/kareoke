ed1d99d5f58c729bb8b014f59f9c52f897adbb6c
lbarcl <ersoybarcin@gmail.com>
2021-05-03 21:42:47 +0300

    200

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const v1 = require('./routes')
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.sendDate()
+    res.sendStatus(200)
 })
 app.use('/v1', v1)
 app.listen(port, () => {console.log(`http://localhost:${port}`)})

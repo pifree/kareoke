3fc6831ab2d29eef538b88244188f84df79ef094
lbarcl <ersoybarcin@gmail.com>
2021-10-19 22:59:54 +0300

    Lyric Fix and Lyric Cache

--- src/routes/api/lyric.js
+++ src/routes/api/lyric.js
@@ -1,29 +1,32 @@
 const router = require('express').Router()
 const cheerio = require('cheerio')
 const fetch = require('node-fetch')
+const cache = require('../../utils/cache')
 
-router.get('/genius', async (req, res) => {
+router.get('/genius', cache(21600000), async (req, res) => {
     if (!req.query.q) return res.status(400).send('You need to send query for search')
     else if (!req.query.type) req.query.type = 'text'
 
     const response = await fetch(`https://api.genius.com/search?q=${req.query.q}`, { headers: { "Authorization": `Bearer nEkoa5hvUWcscXor9utBB8wt6EDXTknzS5lmSjmzmJRNNqPfxLXmbzb7_GzTwZC9` } })
     const data = await response.json()
 
-    if (data.response.hits.length == 0) return res.status(404).send({ 'msg': `Couldn't find a song with ${req.query.q}`, 'status_code': 404 })
-    
+    if (data.meta.status != 200) return res.status(data.meta.status).send({ 'msg': `Couldn't find a song with ${req.query.q}`, 'status_code': 404 })
+
     const htmlResponse = await fetch('https://genius.com' + data.response.hits[0].result.path)
     const htmlPage = await htmlResponse.text()
 
     try {
         let $ = cheerio.load(htmlPage)
+        var lyric
         switch (req.query.type) {
             case 'html':
-                res.send($('div [class="lyrics"]').html())
+                lyric = await $('div.lyrics').html()
                 break;
             case 'text':
-                res.send($('div [class="lyrics"]').text())
+                lyric = await $('div.lyrics').text()
                 break;
-        } 
+        }
+        res.send(lyric)
     } catch (e) {
         res.sendStatus(500)
         console.log(e)

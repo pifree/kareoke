7239773bb8ba5eb2b9b0c5ce6cf66ac90547c6cc
Barçın <68463413+lbarcl@users.noreply.github.com>
2021-12-27 23:49:22 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.use('/api', require('./api'))
+router.use('/v1', require('./api'))
 router.all('/brew/coffe', (req, res) => {
     res.status(418).send({'msg': 'Bro I am a teapot, how the fuck I can brew coffe', 'status_code': 418})
 })
